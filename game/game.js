556ae9bb3f158e0853b95863de2fd301a9bb4639
Katherin_Z <zyrianovakate10@gmail.com>
2020-02-06 23:30:48 +0300

    wip

--- game/game.js
+++ game/game.js
@@ -12,7 +12,7 @@ let hits = 0
 //утонул ли
 let isSunk = false
 
-//пишем цикл, работающий пока корабле не утонул
+//пишем цикл, работающий пока корабль не утонул
 while (isSunk === false) {
   //спрашиваем координату выстрела у игрока
   currentShot = Number(prompt('Enter a number from 0 to 9:'))
@@ -23,16 +23,21 @@ while (isSunk === false) {
   //если координата выстрела совпадает с какой-нибудь координатой корабля, тогда игрок попал
   //Поэтому увеличиваю кол-во попаданий
 
-  if (currentShot === location1 || currentShot === location2 || currentShot === location3){
-    hits = hits + 1
+  if (currentShot === location1 || currentShot === location2|| currentShot === location3){
+    hits = hits + 1}
+     else if (currentShot === location1 ){
+     hits != hits + 1
+   }
+
+  if (currentShot != location1 || location2|| location3){
+    alert('try again!')
   }
 
   //если попал  три раза, топлю корабль
   if (hits === 3) {
     alert('congrats!')
     isSunk === true
-
   }
 
-  
+
 }
