d23e647444b2448f1de7b1139df63db07fcbea3d
Katherin_Z <zyrianovakate10@gmail.com>
2020-02-14 15:42:04 +0300

    wip

--- 04/animations.js
+++ 04/animations.js
@@ -11,6 +11,24 @@ function makeCircle() {
   box.style.borderRadius = "100%"
 }
 function move() {
-  box.style.translateX = "50px"
-
+  box.style.transform = "translate(50px, 50px)"
 }
+function makeBlinking() {
+box.classList.add("blinking")
+}
+
+function addText() {
+    let smth = document.getElementById("myText").value
+  document.getElementById("textInput").innerHTML = smth
+  box.style.color = "white"
+}
+function changeColor() {
+ let name = document.getElementById("myColor").value
+ box.style.backgroundColor = name
+ name.value = ""
+}
+//  ИЛИ ТАК
+// function changeColor() {
+//   let colorInput = document.querySelector("#myColor")
+//   box.style.backgroundColor = colorInput
+// }
